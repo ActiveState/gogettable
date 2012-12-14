49683f26ccde3f8484478a2a45d4085eec61b05b
Sridhar Ratnakumar <sridharr@activestate.com>
2012-12-14 12:38:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+A simple web app to provide `go get` the manifest needed to clone
+internal Go repos.
+
+Example:
+
+    go get gopkg.iprod.activestate.com/stackato-go
---
+++ gopkg.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"code.google.com/p/gorilla/mux"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Fprintf(w, "<html><head>%s</head></html>", getMetaTag(vars["name"]))
+	})
+	http.Handle("/", router)
+
+	addr := ":" + os.Getenv("PORT")
+	log.Println("Listening on " + addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func getMetaTag(name string) string {
+	return fmt.Sprintf("<meta name=\"go-import\" content=\"gopkg.iprod.activestate.com/%s git gitolite@gitolite.activestate.com:%s.git\">",
+		name, name)
+}
